4e923898f66a29644413b49e4d8f80a5b044a82e
Leoncito123 <122043056@upq.edu.mx>
2024-05-27 20:13:10 -0600

    Creacion de rutas y metodos

--- Parcial1/practica3/Flask/app.py
+++ Parcial1/practica3/Flask/app.py
@@ -1,7 +1,34 @@
-from flask import Flask
+from flask import Flask, request
 
 #instacia de la clase Flask
 app = Flask(__name__)
+
+#Ruta basica
+@app.route('/')
+def principal ():
+    return "Hola mundo"
+#Ruta doble
+@app.route('/usuario')
+@app.route('/saludar')
+def saludar():
+    return "Hola usuario"
+
+#Rutas con parametros
+@app.route('/hi/<nombre>')
+def hi(nombre):
+    return 'Hola'+ ' '+ nombre + '!!!'
+
+#Definicon de metodos de trabajo 
+@app.route('/formulario/', methods=['GET','POST'])
+def formulario():
+    if request.method == 'GET':
+        return 'No es seguro enviar password por GET'
+    elif request.metod == 'POST':
+        return 'POST si es seguro para password'
+#Manejo de excepciones en las rutas
+@app.errorhandler(404)
+def pagina_no_encontrada(e):
+    return 'Revisa tu sintaxis:No encontre nada'
 
 #Definir la ruta de la pagina 
 if __name__ == '__main__':
